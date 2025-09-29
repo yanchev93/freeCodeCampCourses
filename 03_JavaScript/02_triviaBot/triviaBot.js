9e84f0dc721d7f9764ff4e0a9bf28f3e9918ef47
Teodor Yanchev <teo.yanchev93@gmail.com>
2025-09-29 17:28:16 -0400

    Second project from the JavaScript certification - freeCodeCamp

---
+++ 03_JavaScript/02_triviaBot/triviaBot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "TriviaBot";
+let botLocation = "the internet";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " Was Created in Just 10 Days";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " Is Not the Same as Java: Despite the similar names, " + favoriteLanguage + " and Java are two entirely different languages";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " Is the Most Popular Language According to Stack Overflow's Developer Survey";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");

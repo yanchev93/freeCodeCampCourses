089aba71aaeb7db8a32ed1640b1dc262009b5975
Teodor Yanchev <teo.yanchev93@gmail.com>
2025-10-15 12:43:47 -0400

    Third project from the JavaScript certification - freeCodeCamp
    The adjective, noun, verb, etc. were totally random.

---
+++ 03_JavaScript/03_sentenceMaker/app.js
@@ -0,0 +1,21 @@
+let adjective = "brave";
+let noun = "acorn";
+let verb = "obliterate";
+let place = "Lourmarin, France";
+let adjective2 = "fearless";
+let noun2 = "cormorant";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "daring";
+noun = "argument";
+verb = "squeezed";
+place = "Barrow, United States";
+adjective2 = "zonked";
+noun2 = "equipment";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`Second story: ${secondStory}`);
